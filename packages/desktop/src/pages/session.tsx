f1da756c62da228fae43b3062992d7e76b07f07a
Ariane Emory <ariane.emory@protonmail.com>
2025-12-12 13:33:40 -0500

    tidy: revert unwanted change.

--- packages/desktop/src/pages/session.tsx
+++ packages/desktop/src/pages/session.tsx
@@ -415,6 +415,7 @@ export default function Page() {
                           messages={session.messages.user()}
                           current={session.messages.active()}
                           onMessageSelect={session.messages.setActive}
+                           working={session.working()}
                           wide={wide()}
                         />
                         <SessionTurn

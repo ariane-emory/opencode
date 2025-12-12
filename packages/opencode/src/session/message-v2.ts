83af7c9978cf54292c80ca94d4cbc980cd1926df
Ariane Emory <ariane.emory@protonmail.com>
2025-12-12 00:58:06 -0500

    fix: uncorrupt

--- packages/opencode/src/session/message-v2.ts
+++ packages/opencode/src/session/message-v2.ts
@@ -275,7 +275,6 @@ export namespace MessageV2 {
     tool: z.string(),
     state: ToolState,
     metadata: z.record(z.string(), z.any()).optional(),
-    hideDetails: z.boolean().optional(),
   }).meta({
     ref: "ToolPart",
   })
